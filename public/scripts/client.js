bb15ca0217d8de3fbc3baecc388be224a268d000
donnyli95 <sz3li@uwaterloo.ca>
2021-04-21 15:41:00 +0000

    implment counter reset after successful tweet submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,13 +7,13 @@
 // create new tweet with proper html tags
 const createTweetElement = (tweetData) => {
   /* Your code for creating the tweet element */
-  let name = tweetData["user"]["name"];
-  let avatar = tweetData["user"]["avatars"];
-  let handle = tweetData["user"]["handle"];
-  let text = tweetData["content"]["text"];
-  let createDate = timeago.format(tweetData["created_at"]);
+  const name = tweetData["user"]["name"];
+  const avatar = tweetData["user"]["avatars"];
+  const handle = tweetData["user"]["handle"];
+  const text = tweetData["content"]["text"];
+  const createDate = timeago.format(tweetData["created_at"]);
 
-  let $tweet = `<section class="existing-tweets">
+  const $tweet = `<section class="existing-tweets">
     <header>
       <div class="header-footer">
         <div class="name-icon">
@@ -34,9 +34,9 @@ const createTweetElement = (tweetData) => {
         </div>
       </div>
     </header>
-  </section>`
+  </section>`;
 
-  return $tweet
+  return $tweet;
 
 };
 
@@ -49,22 +49,22 @@ const renderTweets = function(tweets) {
 
 const loadTweets = () => {
   $.get('/tweets', renderTweets);
-}
+};
 
 
 // Load and render latest tweet
 const loadLastTweet = () => {
-  $.get('/tweets', (tweets) => renderTweets([tweets[tweets.length - 1]]))
-}
+  $.get('/tweets', (tweets) => renderTweets([tweets[tweets.length - 1]]));
+};
 
 //post to backend
 const submitFormData = (form) => {
   let serializedForm = form.serialize();
-  $.post('/tweets', serializedForm, loadLastTweet) 
+  $.post('/tweets', serializedForm, loadLastTweet);
 };
 
 // convert to plain string to avoid XSS
-const escape = function (str) {
+const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
--- public/scripts/document-ready.js
+++ public/scripts/document-ready.js
@@ -6,33 +6,34 @@ $(document).ready(function() {
   $("#tweet-text").on('input', function() {
     let inputLength = $(this).val().length;
     let remaining = 140 - inputLength;
-    let counter = $(this).closest(".container").find(".counter")
+    let counter = $(this).closest(".container").find(".counter");
 
-    if(remaining < 0) {
-      counter
-      .addClass("over140")
+    if (remaining < 0) {
+      counter.addClass("over140");
     } else {
-      counter
-      .removeClass("over140")
+      counter.removeClass("over140");
     }
 
     $("form").find(".counter").val(remaining);
-  })
+  });
 
 
   // submit new tweets
   $(".new-tweet-form").submit(function(event) {
     event.preventDefault();
-
+    // shows appropriate error message while hiding irrelevant ones
     if ($('#tweet-text').val().length < 1) {
-      $(".empty-tweet").toggle();
-    } else if ($('#tweet-text').val().length > 140 ) {
-      $(".too-much-text").toggle();
+      $(".empty-tweet").show();
+      $(".too-much-text").hide();
+    } else if ($('#tweet-text').val().length > 140) {
+      $(".too-much-text").show();
+      $(".empty-tweet").hide();
     } else {
       $(".too-much-text").hide();
       $(".empty-tweet").hide();
       submitFormData($(".new-tweet-form"));
       $("#tweet-text").val('');
+      $(this).closest(".container").find(".counter").val(140);
     }
   });
 });
